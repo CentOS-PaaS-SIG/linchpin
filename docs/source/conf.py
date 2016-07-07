9efd39330b7a7ecbdc94650d94bbb54b404d1003
Samvaran Rallabandi <srallaba@redhat.com>
2016-07-07 13:18:30 -0400

    updated the theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -123,6 +123,15 @@ todo_include_todos = False
 #
 #html_theme = 'alabaster'
 
+
+html_theme = "classic"
+html_theme_options = {
+    "rightsidebar": "true",
+    "relbarbgcolor": "black"
+}
+
+
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
