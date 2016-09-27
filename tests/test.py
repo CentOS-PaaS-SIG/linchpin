36407732b571d16964953e1bdee52b99e84c6840
Samvaran Rallabandi <srallaba@redhat.com>
2016-09-27 16:06:52 -0400

    updated test.py

--- tests/test.py
+++ tests/test.py
@@ -10,5 +10,7 @@ from test_linchpin_creds import TestLinchPinCredentials
 print "Testing Linchpin TestClass: TestLinchpinEnv"
 from test_linchpin_env import TestLinchPinEnv
 
+print "Testing Linchpin TestClass: TestLinchpinInventoryFilters"
+from test_linchpin_inventory_filters import TestLinchPinInventoryFilters 
 import nose
 nose.run()
