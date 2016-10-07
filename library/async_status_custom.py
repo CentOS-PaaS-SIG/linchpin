15413a7f1469c383a5315ad74807371971db2b4b
Samvaran Rallabandi <srallaba@redhat.com>
2016-10-07 16:10:50 -0400

    bugfix: async provision and teardown not generating outputs

--- library/async_status_custom.py
+++ library/async_status_custom.py
@@ -82,7 +82,7 @@ def main():
     data = None
     try:
         data = file(log_path).read()
-        if module_name == 'os_server' or module_name == 'os_volume' and len(data.split("\n")) == 4 :
+        if (module_name == 'os_server' or module_name == 'os_volume') and len(data.split("\n")) == 4 :
           data = open(log_path).readlines()
           data = data[2]
         data = json.loads(data)
