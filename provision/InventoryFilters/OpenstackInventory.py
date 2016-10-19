d576a89689041709e99147f616521089d8b9f634
Clint Savage <herlo@redhat.com>
2016-10-19 09:55:39 -0600

    Initial work for DuffyInventory filter

--- provision/InventoryFilters/OpenstackInventory.py
+++ provision/InventoryFilters/OpenstackInventory.py
@@ -31,8 +31,9 @@ class OpenstackInventory(InventoryFilter):
 
         # calculating number of groups from topology file
         if len(topo['os_server_res'])== 0:
-            return "" 
+            return ""
 
+        # calculating number of groups from topology file
         no_of_groups = len(topo['os_server_res'])
         inven_hosts = self.get_host_ips(topo, async_flag)
         # adding sections to respective host groups
