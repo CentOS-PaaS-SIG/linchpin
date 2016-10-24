33054902cf79786edb154a48e764112f76154b54
Samvaran Rallabandi <srallaba@redhat.com>
2016-10-24 13:38:54 -0400

    bugfix: updated inventory filters : removed check for str & unicode

--- provision/InventoryFilters/DuffyInventory.py
+++ provision/InventoryFilters/DuffyInventory.py
@@ -36,7 +36,7 @@ class DuffyInventory(InventoryFilter):
         return config
 
     def get_inventory(self, topo, layout):
-        if len(topo['duffy_res']) == 0 or type(topo['duffy_res']) in [str, unicode]:
+        if len(topo['duffy_res']) == 0 :
             return ""
         inventory = ConfigParser(allow_no_value=True)
         layout_hosts = self.get_layout_hosts(layout)
