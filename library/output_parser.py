11515bf3ec6badf0e4324a0f7d643b993684010e
Samvaran Rallabandi <srallaba@redhat.com>
2016-11-14 17:14:41 -0500

    pep8 on output_parser.py

--- library/output_parser.py
+++ library/output_parser.py
@@ -4,23 +4,6 @@
 # Author: Samvaran Kashyap Rallabandi -  <srallaba@redhat.com>
 #
 # output parser  for Ansible based infra provsioning tooli, linch-pin
-DOCUMENTATION = '''
----
-version_added: "0.1"
-module: topo_check
-short_description: output_parser module in ansible
-description:
-  - This module allows a user to parser the output yaml file and register it as variable in ansible.
-
-options:
-  output_file:
-    description:
-      path to topology output file can be in json/yaml format
-    required: true
-
-author: Samvaran Kashyap Rallabandi -
-'''
-
 from ansible.module_utils.basic import *
 import datetime
 import sys
@@ -31,35 +14,59 @@ import tempfile
 import yaml
 import jsonschema
 from jsonschema import validate
+from ansible.module_utils.basic import *
+
+DOCUMENTATION = '''
+---
+version_added: "0.1"
+module: output_parser
+short_description: output_parser module in ansible
+description:
+  - This module allows a user to parser the output yaml file
+    and register it as variable in ansible.
+
+options:
+  output_file:
+    description:
+      path to topology output file can be in json/yaml format
+    required: true
+
+author: Samvaran Kashyap Rallabandi -
+'''
 
 
 def check_file_paths(module, *args):
     for file_path in args:
         if not os.path.exists(file_path):
-            module.fail_json(msg= "File not found %s not found" % (file_path))
+            output = "File not found %s not found"
+            module.fail_json(msg=output % (file_path))
         if not os.access(file_path, os.R_OK):
-            module.fail_json(msg= "File not accesible %s not found" % (file_path))
+            output = "File not accesible %s not found"
+            module.fail_json(msg=output % (file_path))
         if os.path.isdir(file_path):
-            module.fail_json(msg= "Recursive directory not supported  %s " % (file_path))
+            output = "Recursive directory not supported  %s "
+            module.fail_json(msg=output % (file_path))
+
 
 def main():
     module = AnsibleModule(
-    argument_spec={
-            'output_file':     {'required': True, 'aliases': ['topology_output_file']},
-        },
-        required_one_of=[],
-        supports_check_mode=True
-    )
+             argument_spec=dict(
+                           output_file=dict(
+                                       required=True,
+                                       aliases=['topology_output_file']
+                           )
+             ),
+             required_one_of=[],
+             supports_check_mode=True)
     data_file_path = os.path.expanduser(module.params['output_file'])
     check_file_paths(module, data_file_path)
-    content = open(data_file_path,"r").read()
+    content = open(data_file_path, "r").read()
     c = yaml.load(content)
-    resp = {"path": data_file_path, "content":c}
+    resp = {"path": data_file_path, "content": c}
     if resp["content"]:
-         changed = True
-         module.exit_json(changed=changed, output=resp)
+        changed = True
+        module.exit_json(changed=changed, output=resp)
     else:
-         module.fail_json(msg=resp)
+        module.fail_json(msg=resp)
 
-from ansible.module_utils.basic import *
 main()
