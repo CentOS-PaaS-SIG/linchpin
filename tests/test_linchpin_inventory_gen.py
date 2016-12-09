3bf3d1867c1a52b6d7a7c1aa2631ab14650e5986
Samvaran Rallabandi <srallaba@redhat.com>
2016-12-09 18:24:32 -0500

    bugfix: updated assert condition of duffy inven_gen

--- tests/test_linchpin_inventory_gen.py
+++ tests/test_linchpin_inventory_gen.py
@@ -145,7 +145,7 @@ class TestLinchPinInventoryGen(object):
                                 options=self.options,
                                 passwords={})
         results = pbex.run()
-        assert_equal(results, 0)
+        assert_equal(results, 1)
 
     @with_setup(setup)
     def test_inventory_other(self):
