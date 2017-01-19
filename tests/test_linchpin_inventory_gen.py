b8e5ac948e24ea5667960669f9068669082279a4
samvarankashyap <srallaba@redhat.com>
2017-01-19 17:32:21 -0500

    updated error code of other inventory

--- tests/test_linchpin_inventory_gen.py
+++ tests/test_linchpin_inventory_gen.py
@@ -160,4 +160,4 @@ class TestLinchPinInventoryGen(object):
                                 options=self.options,
                                 passwords={})
         results = pbex.run()
-        assert_equal(results, 1)
+        assert_equal(results, 2)
