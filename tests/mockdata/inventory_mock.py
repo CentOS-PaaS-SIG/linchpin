6772af7cb499246499d16e1a42d45668b02b748a
samvarankashyap <samvaran.kashyap@gmail.com>
2017-02-08 12:16:04 -0500

    updated the inventory mock data and references

--- tests/mockdata/inventory_mock.py
+++ tests/mockdata/inventory_mock.py
@@ -9,22 +9,17 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 def get_mock_topo_output():
     return json.loads(open(dir_path+"/test_input.json", "r").read())
 
-
 def get_mock_layout():
     return json.loads(open(dir_path+"/test_layout.json", "r").read())
-
 
 def get_mock_pf():
     return yaml.load(open(dir_path+"/PinFile", "r").read())
 
-
 def get_mock_pf_path():
     return dir_path+"/PinFile"
 
-
 def get_mock_outputfile():
-    return dir_path+"/all_stack_outputs.testoutput"
-
+    return dir_path+"/ex_all_output.json"
 
 def get_mock_layoutfile():
     return dir_path+"/openshift-3node-cluster.yml"
