08d29e06a58bff68403afa034e8ce2eb6f353553
samvarankashyap <samvaran.kashyap@gmail.com>
2017-03-09 14:54:21 -0500

    Moved the libvirt teardown playbook, examples and filter plugins to correct places . Deleted the unecessary folders.

---
+++ linchpin/provision/InventoryFilters/LibvirtInventory.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import abc
+import StringIO
+from ansible import errors
+
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+from InventoryFilter import InventoryFilter
+
+
+class LibvirtInventory(InventoryFilter):
+
+    def get_host_ips(self, topo):
+        ips = []
+        if not ('libvirt_res' in topo):
+            return ips
+        for val in topo['libvirt_res']:
+            print('ip: {}'.format(val))
+            ips.append(val)
+        return ips
+
+    def get_inventory(self, topo, layout):
+        if len(topo['libvirt_res']) == 0:
+            return ""
+        inventory = ConfigParser(allow_no_value=True)
+        layout_hosts = self.get_layout_hosts(layout)
+        inven_hosts = self.get_host_ips(topo)
+        # adding sections to respective host groups
+        host_groups = self.get_layout_host_groups(layout)
+        self.add_sections(host_groups)
+        # set children for each host group
+        self.set_children(layout)
+        # set vars for each host group
+        self.set_vars(layout)
+        # add ip addresses to each host
+        self.add_ips_to_groups(inven_hosts, layout)
+        self.add_common_vars(host_groups, layout)
+        output = StringIO.StringIO()
+        self.config.write(output)
+        return output.getvalue()
---
+++ linchpin/provision/filter_plugins/ordered_yaml.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import json
+import yaml
+import yamlordereddictloader
+from camel import Camel
+
+def to_ordered_dict(filepath):
+    data = yaml.load(open(filepath), Loader=yamlordereddictloader.Loader)
+    data = Camel().dump(data["inventory_layout"])
+    return data
+
+class FilterModule(object):
+    ''' A filter to fix interface's name format '''
+    def filters(self):
+        return {
+            'ordered_yaml': to_ordered_dict 
+        }
