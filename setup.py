876497a83607d20e61bd741b3d0f1e60671c6e48
samvarankashyap <srallaba@redhat.com>
2017-03-28 12:54:18 -0400

    Updated schema reference in tests and adjusted setup.py to reflect the optional dependecies w.r.to new layout

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup, find_packages
-#from distutils.core import setup
 from pip.req import parse_requirements
 import os
 
 # reading requirements from requirements.txt
 dir_path = os.path.dirname(os.path.realpath(__file__))
 reqs_file = 'requirements.txt'.format(dir_path)
-
 with open(reqs_file) as f:
     required = f.read().splitlines()
 
@@ -14,7 +12,7 @@ ignore_dir = ['.git']
 
 setup(
     name='linchpin',
-    version='0.8.7',
+    version='1.0.0a1',
     description = 'Ansible based multi cloud orchestrator',
     author = 'samvaran kashyap rallabandi',
     author_email = 'samvaran.kashyap@gmail.com',
@@ -26,13 +24,15 @@ setup(
         linchpin=linchpin:cli
     ''',
     extras_require = {
+        'libvirt': ["libvirt-python>=3.0.0"],
+        'krbV': ["python-krbV"]
         'beaker':  ['beaker-client==23.3'],
         'docs': ["docutils","sphinx","sphinx_rtd_theme"],
         'tests': ["nose","mock","coverage"],
     },
+    dependency_links = ['https://github.com/eevee/camel/tree/v0.1/tarball/'],
     zip_safe=False,
     packages=find_packages(),
     include_package_data=True,
-
     scripts=['scripts/linchpin_complete.sh']
 )
