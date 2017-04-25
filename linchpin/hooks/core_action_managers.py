bf8f3a9081cfcbc14bc8579b15d55a8c2db1c893
samvarankashyap <srallaba@redhat.com>
2017-04-25 14:26:45 -0400

    Added core_action_managers containing all available action_managers
    Contains a dictionary of actionmanagers and their class references
    used by ActionBlockRouter in hooks/__init__.py

---
+++ linchpin/hooks/core_action_managers.py
@@ -0,0 +1,13 @@
+from subprocess_action_manager import SubprocessActionManager
+from ansible_action_manager import AnsibleActionManager
+from python_action_manager import PythonActionManager
+from ruby_action_manager import RubyActionManager
+from node_action_manager import NodeActionManager
+
+ACTION_MANAGERS = {
+    "shell" : SubprocessActionManager,
+    "ansible" : AnsibleActionManager,
+    "python": PythonActionManager,
+    "ruby" : RubyActionManager,
+    "node" : NodeActionManager
+}
