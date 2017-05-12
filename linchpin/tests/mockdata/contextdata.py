6037e9f866dacabd248db13da9722ed3ff59fd3e
Clint Savage <herlo1@gmail.com>
2017-05-12 16:38:59 -0600

    Load contextdata from a test linchpin.conf
    Initially, this test data used a dict, but that got unruly. Now using a
    linchpin.conf modeled after the one in the linchpin lib dir.

--- linchpin/tests/mockdata/contextdata.py
+++ linchpin/tests/mockdata/contextdata.py
@@ -15,53 +15,80 @@ class ContextData(object):
 
     def __init__(self, parser=ConfigParser.SafeConfigParser):
 
-        """
-        Create a dictionary to represent an ini-style configuration file
 
-        """
+        self.lib_path = '{0}'.format(os.path.dirname(
+            os.path.realpath(__file__))).rstrip('/')
 
         self.logfile = tempfile.mktemp(suffix='.log', prefix='linchpin', dir='/tmp')
         self.parser = parser()
 
-        self.config_data = {
-            'console': {
-                'enable': 'True', # placeholder
-            },
-            'logger': {
-                'enable': 'True',
-                'file': self.logfile,
-            },
-            'playbooks': {
-                'up': 'up.yml',
-                'destroy': 'destroy.yml',
-                'down': 'down.yml',
-                'schema_check': 'schemacheck.yml',
-                'inv_gen': 'invgen.yml',
-                'test': 'test.yml'
-            },
-            'lp': {
-                'module_folder': 'modules',
-                'pf_excludes': 'upstream1,upstream2'
-            },
-        }
 
-        self.evars = {
-                'async': False,
-                'async_timeout': '1000',
-                'output': True,
-                'check_mode': False
-        }
+    def load_config_data(self, provider='dummy'):
+        """
+        Load a test-based linchpin.conf into both a configs and evars
+        dictionary to represent a configuration file
+
+        """
+
+        self.cfg_data = {}
+
+        expanded_path = None
+        config_found = False
+
+        # simply modify this variable to adjust where linchpin.conf can be found
+        CONFIG_PATH = [
+            '{0}/{1}/conf/linchpin.conf'.format(self.lib_path, provider)
+        ]
+
+        for path in CONFIG_PATH:
+            expanded_path = (
+                "{0}".format(os.path.realpath(os.path.expanduser(path))))
+
+            # implement first found
+            if os.path.exists(expanded_path):
+                config_found = True
+                break
+
+        if not config_found:
+            raise LinchpinError('Configuration file not found in'
+                                ' path: {0}'.format(CONFIG_PATH))
+
+        config = ConfigParser.SafeConfigParser()
+        try:
+            f = open(path)
+            config.readfp(f)
+            f.close()
+        except ConfigParser.InterpolationSyntaxError as e:
+            raise LinchpinError('Unable to parse configuration file properly:'
+                    ' {0}'.format(e))
+
+        for section in config.sections():
+            if section not in self.cfg_data:
+                self.cfg_data[section] = {}
+
+            # add evars to the ansible extra_vars when running a playbook.
+            for k, v in config.items(section):
+                if section != 'evars':
+                    self.cfg_data[section][k] = v
+                else:
+                    try:
+                        self.cfg_data[section][k] = config.getboolean(section, k)
+                    except ValueError as e:
+                        self.cfg_data[section][k] = v
+
+        self.evars = self.cfg_data.get('evars', {})
 
 
     def get_temp_filename(self):
 
-        return tempfile.NamedTemporaryFile(delete=False).name
+        tmpfile = tempfile.NamedTemporaryFile(delete=False).name
+        return tmpfile
 
 
     def write_config_file(self, path):
 
         try:
-            with open(path, 'w+') as f:
+            with open(path, 'a+') as f:
                 self.parser.write(f)
         except Exception as e:
             raise LinchpinError('Unable to write configuration file:'
@@ -76,27 +103,13 @@ class ContextData(object):
         """
 
         if not config_data:
-            config_data = self.config_data
+            config_data = self.cfg_data
 
         #we know that data is a dict, containing dicts
         for k, v in config_data.iteritems():
             self.parser.add_section(k)
             for kv, vv in v.iteritems():
+                if type(vv) is not str:
+                    vv = str(vv)
                 self.parser.set(k, kv, vv)
-
-
-    def create_context_evars(self):
-
-        """
-        Adds extra_vars (evars) to a config object
-
-        .. note:: To use thie function with boolean objects, RawConfigParser
-                  is required
-
-        """
-
-        self.parser.add_section('evars')
-        for k, v in self.evars.iteritems():
-            self.parser.set('evars', k, v)
-
 
