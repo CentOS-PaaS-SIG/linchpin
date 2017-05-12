29a108eef652007d30709bb9a4c2ada074cee1e2
samvarankashyap <srallaba@redhat.com>
2017-05-12 18:51:36 -0400

    Added provide_default filter to provide default values if value is null string
    Current built in default jinja filter helps the user to omit/provide default to keys
    in the ansible module. However , it doesnt provide a way for user to conditionally
    provide a default value to ie., based on the value provided by the output of previous
    filter . In order to facilitate the same provide_default filter is being written.

---
+++ linchpin/provision/filter_plugins/provide_default.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+
+def provide_default(fetched, default):
+    if fetched == "":
+        return default
+
+class FilterModule(object):
+    ''' A filter to fix interface's name format '''
+    def filters(self):
+        return {
+            'provide_default': provide_default
+        }
