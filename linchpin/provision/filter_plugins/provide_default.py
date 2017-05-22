df29db986c177e34dde4e26b239424e66106f168
samvarankashyap <srallaba@redhat.com>
2017-05-22 16:36:43 -0400

    bugfix: Make provide default return the fetched items

--- linchpin/provision/filter_plugins/provide_default.py
+++ linchpin/provision/filter_plugins/provide_default.py
@@ -4,6 +4,8 @@ import os
 def provide_default(fetched, default):
     if fetched == "":
         return default
+    else:
+        return fetched
 
 class FilterModule(object):
     ''' A filter to fix interface's name format '''
