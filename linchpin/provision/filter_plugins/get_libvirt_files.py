d9216766969b580a21bcd7411fc93e153c978088
samvarankashyap <srallaba@redhat.com>
2017-06-19 15:02:46 -0400

    Added functionality to remove images after the teardown .
    Current commit removes all the images created by linchpin up .
    get_libvirt_files filter is added to parse the results of xmldumps to
    fetch the image sources.

---
+++ linchpin/provision/filter_plugins/get_libvirt_files.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+import json
+from ansible import errors
+from xml.etree.ElementTree import XML, fromstring, tostring
+
+def get_libvirt_files(output):
+    files = []
+    results = output['results']
+    for result in results:
+        stdout = result['stdout']
+        myxml = fromstring(stdout)
+        devices = myxml.findall('devices')
+        for device in devices:
+            disks = device.findall('disk')
+            for disk in disks:
+                if disk.attrib["type"] == 'file':
+                    source = disk.findall('source')[0]
+                    files.append(source.attrib['file'])
+    return files
+
+class FilterModule(object):
+    ''' A filter to fix network format '''
+    def filters(self):
+        return {
+            'get_libvirt_files': get_libvirt_files
+        }
