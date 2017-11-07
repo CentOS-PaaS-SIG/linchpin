8b70fd6d14008bbb843ca558ebcf14447f9a0912
samvarankashyap <srallaba@redhat.com>
2017-11-07 13:04:30 -0500

    Updated scripts for installing libvirt dependencies and installing selinux in venvs

--- scripts/install_libvirt_deps.sh
+++ scripts/install_libvirt_deps.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-  echo "Usage $0 <lp-path>"
-  echo
-  echo lp-path: path to linchpin source
-  exit 1
-fi
-
-LP_PATH=${1}
-
-VENV_LIB_PATH=lib/python2.7/site-packages
-LIBSELINUX_PATH=/usr/lib64/python2.7/site-packages
-
-if [ -n "${VIRTUAL_ENV}" ]; then
-
-    if [ "${USER}" != "root" ]; then
-        sudo dnf install libvirt-devel libselinux-python -yq &> /dev/null
-    else
-        dnf install libvirt-devel libselinux-python -yq &> /dev/null
-    fi
-
-    ln -s ${LIBSELINUX_PATH}/selinux ${VIRTUAL_ENV}/${VENV_LIB_PATH} &> /dev/null
-    ln -s ${LIBSELINUX_PATH}/_selinux.so ${VIRTUAL_ENV}/${VENV_LIB_PATH} &> /dev/null
-
-    pip install ${LP_PATH}\[libvirt\]
+dnf install libvirt-devel -yq
+dnf install libguestfs-tools python-libguestfs
+pip install linchpin[libvirt]
 
 
-else
-
-    echo "A virtual environment is recommended"
-    exit 1
-
-fi
---
+++ scripts/install_selinux_venv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VENV_LIB_PATH=lib/python2.7/site-packages
+LIBSELINUX_PATH=/usr/lib64/python2.7/site-packages
+
+if [ -n "${VIRTUAL_ENV}" ]; then
+    dnf install libvirt-devel libselinux-python -yq &> /dev/null
+    ln -s ${LIBSELINUX_PATH}/selinux ${VIRTUAL_ENV}/${VENV_LIB_PATH} &> /dev/null
+    ln -s ${LIBSELINUX_PATH}/_selinux.so ${VIRTUAL_ENV}/${VENV_LIB_PATH} &> /dev/null
+else
+    echo "A virtual environment is recommended"
+    dnf install libvirt-devel libselinux-python -yq &> /dev/null
+    exit 1
+
+fi
