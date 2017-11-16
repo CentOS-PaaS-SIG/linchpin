9ca1f2c1852dcee69d666d3ef3d2356d5b0b6100
Bill Peck <bpeck@redhat.com>
2017-11-16 11:47:10 -0500

    Change order from newest distros to oldest.
    Add in duffy but conditionally if duffy.key exists.
    linchpin down was missing

--- Dockerfiles/run_tests.sh
+++ Dockerfiles/run_tests.sh
@@ -23,5 +23,9 @@ docker run --privileged -d -v $LINCHPINDIR:/workdir/ \
     -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name $CONTAINER $CONTAINER
 docker exec -it $CONTAINER /root/linchpin-install.sh
 for i in $DRIVERS; do
+    if [ "$i" = "duffy" -a ! -e "$HOME/duffy.key" ]; then
+        (>&2 echo "WARN: skipping duffy test since $HOME/duffy.key is missing")
+        continue
+    fi
     docker exec -it $CONTAINER /root/linchpin-test.sh $i
 done
