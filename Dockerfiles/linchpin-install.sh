e92629328fd694023cf8416ab6b4a52e1daf6223
Bill Peck <bpeck@redhat.com>
2017-11-17 13:01:43 -0500

    Fixes to run duffy

--- Dockerfiles/linchpin-install.sh
+++ Dockerfiles/linchpin-install.sh
@@ -1,6 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
 
 pushd /workdir
 pip install .
 pip install .[tests]
 pip install .[libvirt]
+popd
+
+# If duffy.key is available then install duffy ansible module.
+if [ -e "/workdir/Dockerfiles/duffy.key" ]; then
+    # duffy key needs to be in home dir by default
+    cp /workdir/Dockerfiles/duffy.key ~
+
+    # Link duffy module linchpin library
+    linchpin_path=$(python -c 'import os, linchpin; print os.path.dirname(linchpin.__file__)')
+    if [ -n "$linchpin_path" ]; then
+        pushd $linchpin_path/provision/library
+        ln -s /workdir/Dockerfiles/duffy-ansible-module/library/duffy.py .
+        popd
+    fi
+fi
--- Dockerfiles/run_tests.sh
+++ Dockerfiles/run_tests.sh
@@ -13,18 +13,28 @@ function clean_up {
 
 trap clean_up EXIT SIGHUP SIGINT SIGTERM
 
+# Pull latest example topolgies
 if [ -d "lp_test_workspace" ]; then
     pushd lp_test_workspace && git pull
     popd
 else
     git clone https://github.com/herlo/lp_test_workspace.git
 fi
+
+# Pull down duffy-ansible-module
+if [ -d "duffy-ansible-module" ]; then
+    pushd duffy-ansible-module && git pull
+    popd
+else
+    git clone https://github.com/CentOS-PaaS-SIG/duffy-ansible-module.git
+fi
+
 docker run --privileged -d -v $LINCHPINDIR:/workdir/ \
     -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name $CONTAINER $CONTAINER
 docker exec -it $CONTAINER /root/linchpin-install.sh
 for i in $DRIVERS; do
-    if [ "$i" = "duffy" -a ! -e "$HOME/duffy.key" ]; then
-        (>&2 echo "WARN: skipping duffy test since $HOME/duffy.key is missing")
+    if [ "$i" = "duffy" -a ! -e "duffy.key" ]; then
+        (>&2 echo "WARN: skipping duffy test since duffy.key is missing")
         continue
     fi
     docker exec -it $CONTAINER /root/linchpin-test.sh $i
