8d71123b4972785fbbe127b818d045d4c798cc06
Clint Savage <herlo1@gmail.com>
2017-12-09 23:45:55 -0700

    Move utils.py to its own namespace, add dataparser
    Since utils will now process inputs, make it an object
    put it into dataparser.py and create an instance DataParser
    if it's to be used.
    
    Added jinja template rendering with process and render methods
    Reworked parse_json_yaml to take string data

---
+++ linchpin/utils/dataparser.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+import sys
+import yaml
+import json
+import subprocess
+
+from jinja2 import BaseLoader
+from jinja2 import Environment
+
+from linchpin.exceptions import LinchpinError
+from linchpin.exceptions import ValidationError
+
+
+def dict_representer(dumper, data):
+    return dumper.represent_mapping(_mapping_tag, data.iteritems())
+
+
+def dict_constructor(loader, node):
+    return dict(loader.construct_pairs(node))
+
+
+class DataParser(object):
+
+
+    def __init__(self):
+
+        self._mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+
+    def process(self, pf_w_path, data_w_path=None):
+        """ Processes the PinFile and any data (if a template)
+        using Jinja2. Returns json of PinFile, topology, layout,
+        and hooks data
+
+        :param pf_w_path:
+            Full path to the provided Pinfile
+
+        :param targets:
+            A tuple of targets to provision
+
+        :param run_id:
+            An optional run_id if the task is idempotent or a destroy action
+        """
+
+        with open(pf_w_path, 'r') as stream:
+            pinfile_data = stream.read()
+
+            if data_w_path:
+                with open(data_w_path, 'r') as strm:
+                    pf_data = strm.read()
+                    pinfile_data = self.render(pinfile_data, pf_data)
+                    return self.parse_json_yaml(pinfile_data)
+
+        return self.load_pinfile(pf_w_path)
+
+
+    def render(self, template, context):
+        """
+        Performs the rendering of template and context data using
+        Jinja2.
+
+        :param template:
+            Full path to the Jinja2 template
+
+        :param context:
+            A dictionary of variables to be rendered againt the template
+        """
+
+        c = self.parse_json_yaml(context)
+        t = Environment(loader=BaseLoader).from_string(template)
+        return t.render(c)
+
+
+    def parse_json_yaml(self, data):
+
+        """ parses yaml file into json object """
+
+        d = None
+
+        # Setup support for ordered dicts so we do not lose ordering
+        # when importing from YAML
+        yaml.add_representer(dict, dict_representer)
+        yaml.add_constructor(self._mapping_tag, dict_constructor)
+
+        data = yaml.load(data)
+
+        if isinstance(data, dict):
+            return data
+
+        return d
+
+
+    def run_script(self, script):
+
+        sp = None
+        try:
+            sp = subprocess.Popen(script,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+        except OSError as e:
+            raise ValidationError("problem running {0} ({1})".format(script, e))
+
+        (stdout, stderr) = sp.communicate()
+
+        if sp.returncode != 0:
+            raise ValidationError("Script {0} had execution error"
+                                  " ({1})".format(script, e))
+
+        return stdout
+
+
+    def load_pinfile(self, pinfile):
+        # try to convert the data into json
+        pf = None
+        try:
+            import pdb
+            pdb.set_trace()
+            with open(pinfile, 'r') as stream:
+                pf_data = stream.read()
+                pf = self.parse_json_yaml(pf_data)
+        except ValidationError as e:
+            pass
+
+        if not pf:
+            # assume not json, so dynamic script
+            # once executed, verify it is json
+            try:
+                pf = json.loads(self.run_script(pinfile))
+            except Exception as e:
+                raise LinchpinError(e)
+
+        return pf
