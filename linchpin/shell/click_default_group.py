80833d1d91c9bbf14830e4dbcd255771dda6e89b
Clint Savage <herlo@redhat.com>
2018-02-01 09:41:47 -0700

    fixup comment in click_default_group.py

--- linchpin/shell/click_default_group.py
+++ linchpin/shell/click_default_group.py
@@ -1,68 +1,69 @@
 # -*- coding: utf-8 -*-
 
-#"""
-#   click_default_group
-#   ~~~~~~~~~~~~~~~~~~~
-#   Define a default subcommand by `default=True`:
-#   .. sourcecode:: python
-#      import click
-#      from click_default_group import DefaultGroup
-#      @click.group(cls=DefaultGroup, default_if_no_args=True)
-#      def cli():
-#          pass
-#      @cli.command(default=True)
-#      def foo():
-#          click.echo('foo')
-#      @cli.command()
-#      def bar():
-#          click.echo('bar')
-#   Then you can invoke that without explicit subcommand name:
-#   .. sourcecode:: console
-#      $ cli.py --help
-#      Usage: cli.py [OPTIONS] COMMAND [ARGS]...
-#      Options:
-#        --help    Show this message and exit.
-#      Command:
-#        foo*
-#        bar
-#      $ cli.py
-#      foo
-#      $ cli.py foo
-#      foo
-#      $ cli.py bar
-#      bar
+# """
+#    click_default_group
+#    ~~~~~~~~~~~~~~~~~~~
+#    Define a default subcommand by `default=True`:
+#    .. sourcecode:: python
+#       import click
+#       from click_default_group import DefaultGroup
+#       @click.group(cls=DefaultGroup, default_if_no_args=True)
+#       def cli():
+#           pass
+#       @cli.command(default=True)
+#       def foo():
+#           click.echo('foo')
+#       @cli.command()
+#       def bar():
+#           click.echo('bar')
+#    Then you can invoke that without explicit subcommand name:
+#    .. sourcecode:: console
+#       $ cli.py --help
+#       Usage: cli.py [OPTIONS] COMMAND [ARGS]...
+#       Options:
+#         --help    Show this message and exit.
+#       Command:
+#         foo*
+#         bar
+#       $ cli.py
+#       foo
+#       $ cli.py foo
+#       foo
+#       $ cli.py bar
+#       bar
 #
-#Credit: Heungsub Lee <https://github.com/sublee>
+# Credit: Heungsub Lee <https://github.com/sublee>
 #
-#Copyright (c) 2015-2016, Heungsub Lee
-#All rights reserved.
+# Copyright (c) 2015-2016, Heungsub Lee
+# All rights reserved.
 #
-#Redistribution and use in source and binary forms, with or without modification,
-#are permitted provided that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without
+# modification,are permitted provided that the following conditions are met:
 #
-#  Redistributions of source code must retain the above copyright notice, this
-#  list of conditions and the following disclaimer.
+#   Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
 #
-#  Redistributions in binary form must reproduce the above copyright notice, this
-#  list of conditions and the following disclaimer in the documentation and/or
-#  other materials provided with the distribution.
+#   Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
 #
-#  Neither the name of the copyright holder nor the names of its
-#  contributors may be used to endorse or promote products derived from
-#  this software without specific prior written permission.
+#   Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
 #
-#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-#ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-#ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-#ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 #
-#"""
+# """
 
 import warnings
 import click
@@ -77,7 +78,8 @@ class DefaultGroup(click.Group):
     Invokes a subcommand marked with `default=True` if any subcommand not
     chosen.
 
-    :param default_if_no_args: resolves to the default command if no arguments passed.
+    :param default_if_no_args: resolves to the default command if no arguments
+                               passed.
     """
 
     def __init__(self, *args, **kwargs):
