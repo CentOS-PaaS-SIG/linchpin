a2d57e2645a130cba3b106b19e9ae6fd6b55a47c
Bill Peck <bpeck@redhat.com>
2018-03-21 10:08:39 -0400

    Enable testing of aws-ec2-new driver.
    define the credentials file
    Fix currentStage variable to be defined in scope.

--- config/Dockerfiles/linchpin-tests.sh
+++ config/Dockerfiles/linchpin-tests.sh
@@ -4,18 +4,26 @@ base_dir="$( pwd )"
 
 set -o pipefail
 
-mkdir -p ${base_dir}/logs
+mkdir -p ${base_dir}/${target}_logs
 
 result=0
 for i in $DRIVERS; do
     testname="$target/$i"
     export CREDS_PATH="$base_dir/keys/$i"
-    if [ ! -e "$CREDS_PATH" ]; then
+    # Horrible hacks until CentOS support is fixed..
+    # See Issue: https://github.com/CentOS-PaaS-SIG/duffy-ansible-module/issues/3
+    if [ "$target" = "centos6" -o "$target" = "centos7" ] && \
+       [ "$i" = "aws-ec2-new" ]; then
         test_summary="$(tput setaf 4)SKIPPED$(tput sgr0)\t${testname}"
         summary="${summary}\n${test_summary}"
         continue
     fi
-    ./config/Dockerfiles/linchpin-test.sh $i 2>&1 |tee logs/${target}_${i}.log
+    if [ "$target" = "centos6" -a "$i" = "duffy" ]; then
+        test_summary="$(tput setaf 4)SKIPPED$(tput sgr0)\t${testname}"
+        summary="${summary}\n${test_summary}"
+        continue
+    fi
+    ./config/Dockerfiles/linchpin-test.sh $i 2>&1 |tee ${target}_logs/${i}.log
     if [ $? -eq 0 ]; then
         test_summary="$(tput setaf 2)SUCCESS$(tput sgr0)\t${testname}"
     else
