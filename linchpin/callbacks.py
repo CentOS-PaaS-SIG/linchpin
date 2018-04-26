3d6439fefb92c5249fabbd27228feeca795835b8
samvaran kashyap rallabandi <srallaba@redhat.com>
2018-04-26 14:20:50 -0400

    Bugfix: Making linchpin work with Ansible 2.5.x
    PlaybookCallback defined does not have attribute _loader_name which is
    required by the ansible executor.py is added in constructor before the
    super call. Further, reverted ansible version in requirements.txt
    back to >=2.3.1.

--- linchpin/callbacks.py
+++ linchpin/callbacks.py
@@ -6,9 +6,23 @@ class PlaybookCallback(CallbackBase):
     """Playbook callback"""
 
 
-    def __init__(self, display=None, options=None):
-        super(PlaybookCallback, self).__init__(display=display,
-                                               options=options)
+    def __init__(self, display=None, options=None, ansible_version=2.3):
+
+        # note the following if else ladder should be restructured after
+        # linchpin ansible minimum requirements changed until then
+        # code should be remained unchanged to maintain backward
+        # compatibility to ansible 2.3.1 version
+        if ansible_version >= 2.5:
+            self._load_name = None
+            super(PlaybookCallback, self).__init__(display=display,
+                                                   options=options)
+        elif ansible_version >= 2.4:
+            super(PlaybookCallback, self).__init__(display=display,
+                                                   options=options)
+        else:
+            # since ansible 2.3.1 version does not support options in
+            # inside PlaybbookCallback the options are omitted
+            super(PlaybookCallback, self).__init__(display=display)
 
         self._options = options
         self._display.verbosity = options.verbosity
