1ba5a2794a550386450da3ce6f6be060c4e126c7
Clint Savage <herlo@redhat.com>
2018-05-14 14:07:48 -0600

    Add dummy.tests file & corresponding test scripts

---
+++ config/Dockerfiles/tests.d/dummy/template-data-file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -xe
+
+DISTRO=${1}
+PROVIDER=${2}
+
+function clean_up {
+    set +e
+    linchpin -w . -p PinFile.${PROVIDER}.yml --template-data @./dummy-data.yml -v destroy dummy-new
+}
+trap clean_up EXIT SIGHUP SIGINT SIGTERM
+
+pushd docs/source/examples/workspace
+cat << EOF > dummy-data.yml
+---
+distro: ${DISTRO}
+EOF
+
+if [ -e /tmp/dummy.hosts ]; then
+    rm /tmp/dummy.hosts
+fi
+
+linchpin -w . -p PinFile.${PROVIDER}.yml --template-data @dummy-data.yml -v up dummy-new
+
+cat /tmp/dummy.hosts | grep "${DISTRO}"
+
---
+++ config/Dockerfiles/tests.d/dummy/template-data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+DISTRO=${1}
+PROVIDER=${2}
+
+function clean_up {
+    set +e
+    linchpin -w . -p PinFile.dummy.yml --template-data "{\"distro\": \"${DISTRO}-\"}" -v destroy dummy-new
+}
+trap clean_up EXIT SIGHUP SIGINT SIGTERM
+
+if [ -e /tmp/dummy.hosts ]; then
+    rm /tmp/dummy.hosts
+fi
+
+echo "DISTRO: ${DISTRO}"
+pushd docs/source/examples/workspace
+linchpin -w . -p PinFile.dummy.yml --template-data "{\"distro\": \"${DISTRO}-\"}" -v up dummy-new
+
+cat /tmp/dummy.hosts | grep "${DISTRO}"
+
