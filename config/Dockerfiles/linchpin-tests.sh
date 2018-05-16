96853da8156c4f291644d63d611a85143400f264
Clint Savage <herlo@redhat.com>
2018-05-16 16:12:41 -0600

    Add a general directory, test loop, and ignore

--- config/Dockerfiles/linchpin-tests.sh
+++ config/Dockerfiles/linchpin-tests.sh
@@ -14,6 +14,7 @@ fi
 
 echo "Testing providers: ${PROVIDERS}"
 
+summary=''
 result=0
 for provider in $PROVIDERS; do
     export CREDS_PATH="$base_dir/keys/${provider}"
@@ -25,17 +26,46 @@ for provider in $PROVIDERS; do
         $tmpdir/install.sh
     fi
     > ${distro}_logs/${provider}.log
-    if [ "$(ls -A ${TEST_DIR}/${provider})" ]; then
+
+    # run generic tests by passing in the provider and distro
+    # should be enough to test the 'basic' provision/teardown
+
+
+    for testfile in ${TEST_DIR}/general/*; do
+        filename=$(basename -- "$testfile")
+        test="general/${filename%.*}"
+        testname=${distro}/${test}
+
+        ignore=''
+        if [ -e "${TEST_DIR}/${distro}-${provider}.ignore" ]; then
+            ignore=$(grep ${test} ${TEST_DIR}/${distro}-${provider}.ignore | grep -v '^#')
+        fi
+
+        if [ -z "${ignore}" ]; then
+            ${TEST_DIR}/general/${filename} ${distro} ${provider} 2>&1 | tee -a ${base_dir}/${distro}_logs/${provider}.log
+            if [ $? -eq 0 ]; then
+                test_summary="$(tput setaf 2)SUCCESS$(tput sgr0)\t${testname}"
+            else
+                test_summary="$(tput setaf 1)FAILURE$(tput sgr0)\t${testname}\tlog: logs/${distro}_logs/_${provider}.log"
+                result=1
+            fi
+            summary="${summary}\n${test_summary}"
+        fi
+    done
+
+    if [ -d "${TEST_DIR}/${provider}" ]; then
         for testfile in ${TEST_DIR}/${provider}/*; do
             filename=$(basename -- "$testfile")
             test="${filename%.*}"
+
             ignore=''
-            if [ ! -z "${TEST_DIR}/${distro}-${provider}.ignore" ]; then
+            if [ -e "${TEST_DIR}/${distro}-${provider}.ignore" ]; then
                 ignore=$(grep ${test} ${TEST_DIR}/${distro}-${provider}.ignore | grep -v '^#')
             fi
+
             if [ -z "${ignore}" ]; then
                 testname="${distro}/${provider}/${test}"
-                ./config/Dockerfiles/tests.d/${provider}/${test} ${distro} ${provider} 2>&1 | tee -a ${base_dir}/${distro}_logs/${provider}.log
+                ${TEST_DIR}/${provider}/${test} ${distro} ${provider} 2>&1 | tee -a ${base_dir}/${distro}_logs/${provider}.log
                 if [ $? -eq 0 ]; then
                     test_summary="$(tput setaf 2)SUCCESS$(tput sgr0)\t${testname}"
                 else
