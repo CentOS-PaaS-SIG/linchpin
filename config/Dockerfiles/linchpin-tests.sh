0dfd1057126fc28587c60226f96540532392fa8b
Clint Savage <herlo@redhat.com>
2018-05-16 13:51:28 -0600

    Get libvirt and dummy working
    Only test dummy and libvirt for now
    Add missing quotes
    add PinFile.libvirt.yml, clean up PinFile.dummy.yml
    include one libvirt test for now
    minor adjustments to scripts

--- config/Dockerfiles/linchpin-tests.sh
+++ config/Dockerfiles/linchpin-tests.sh
@@ -9,7 +9,7 @@ mkdir -p ${base_dir}/${distro}_logs
 TEST_DIR="./config/Dockerfiles/tests.d"
 
 if [ -z "${PROVIDERS}" ]; then
-    PROVIDERS="dummy openstack"
+    PROVIDERS="dummy libvirt"
 fi
 
 echo "Testing providers: ${PROVIDERS}"
@@ -25,25 +25,27 @@ for provider in $PROVIDERS; do
         $tmpdir/install.sh
     fi
     > ${distro}_logs/${provider}.log
-    for testfile in ${TEST_DIR}/${provider}/*; do
-        filename=$(basename -- "$testfile")
-        test="${filename%.*}"
-        ignore=''
-        if [ ! -z "${TEST_DIR}/${distro}-${provider}.ignore" ]; then
-            ignore=$(grep ${test} ${TEST_DIR}/${distro}-${provider}.ignore | grep -v '^#')
-        fi
-        if [ -z "${ignore}" ]; then
-            testname="${distro}/${provider}/${test}"
-            ./config/Dockerfiles/tests.d/${provider}/${test} ${distro} ${provider} 2>&1 | tee -a ${base_dir}/${distro}_logs/${provider}.log
-            if [ $? -eq 0 ]; then
-                test_summary="$(tput setaf 2)SUCCESS$(tput sgr0)\t${testname}"
-            else
-                test_summary="$(tput setaf 1)FAILURE$(tput sgr0)\t${testname}\tlog: logs/${distro}_logs/_${provider}.log"
-                result=1
+    if [ "$(ls -A ${TEST_DIR}/${provider})" ]; then
+        for testfile in ${TEST_DIR}/${provider}/*; do
+            filename=$(basename -- "$testfile")
+            test="${filename%.*}"
+            ignore=''
+            if [ ! -z "${TEST_DIR}/${distro}-${provider}.ignore" ]; then
+                ignore=$(grep ${test} ${TEST_DIR}/${distro}-${provider}.ignore | grep -v '^#')
             fi
-            summary="${summary}\n${test_summary}"
-        fi
-    done
+            if [ -z "${ignore}" ]; then
+                testname="${distro}/${provider}/${test}"
+                ./config/Dockerfiles/tests.d/${provider}/${test} ${distro} ${provider} 2>&1 | tee -a ${base_dir}/${distro}_logs/${provider}.log
+                if [ $? -eq 0 ]; then
+                    test_summary="$(tput setaf 2)SUCCESS$(tput sgr0)\t${testname}"
+                else
+                    test_summary="$(tput setaf 1)FAILURE$(tput sgr0)\t${testname}\tlog: logs/${distro}_logs/_${provider}.log"
+                    result=1
+                fi
+                summary="${summary}\n${test_summary}"
+            fi
+        done
+    fi
 done
 
 printf "\n==== TEST summary ====${summary}\n"
