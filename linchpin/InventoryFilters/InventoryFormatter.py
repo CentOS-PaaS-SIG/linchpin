68d4eaade558116882dec0a5ea04da257ee5b154
Samvaran Kashyap Rallabandi <samvaran.kashyap@gmail.com>
2018-05-23 14:24:08 -0400

    Added InventoryFormatters and util functions to fetch them

---
+++ linchpin/InventoryFilters/InventoryFormatter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import abc
+import StringIO
+
+from InventoryFilter import InventoryFilter
+
+
+class InventoryFormatter:
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def add_sections(self, section_list):
+        pass
+
+    @abc.abstractmethod
+    def set_children(self, inv):
+        pass
+
+    @abc.abstractmethod
+    def set_vars(self, inv):
+        pass
+
+    @abc.abstractmethod
+    def add_ips_to_groups(self, inven_hosts, layout):
+        pass
+
+    @abc.abstractmethod
+    def add_common_vars(self, host_groups, layout):
+        pass
+
+    @abc.abstractmethod
+    def generate_inventory(self):
+        pass
