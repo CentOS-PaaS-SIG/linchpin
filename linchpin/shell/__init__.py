7d10f3b6cfee90686cb3dcfed1cb47b1524cec9e
Samvaran Kashyap Rallabandi <samvaran.kashyap@gmail.com>
2018-10-03 10:37:52 -0400

    Revert back to -- notation for flags

--- linchpin/shell/__init__.py
+++ linchpin/shell/__init__.py
@@ -208,11 +208,10 @@ def init(ctx):
               cls=MutuallyExclusiveOption, mutually_exclusive=["run_id"])
 @click.option('-if', '--inventory-format', default="cfg",
               help="Inventory format can be cfg or json")
-@click.option('-ifh', '--ignore-failed-hooks', is_flag=True, default=None)
-@click.option('-nh', '--no-hooks', is_flag=True, default=None)
+@click.option('--ifh', '--ignore-failed-hooks', is_flag=True, default=None)
+@click.option('--nh', '--no-hooks', is_flag=True, default=None)
 @pass_context
-def up(ctx, targets, run_id, tx_id, inventory_format,
-       ignore_failed_hooks, no_hooks):
+def up(ctx, targets, run_id, tx_id, inventory_format, ifh, nh):
     """
     Provisions nodes from the given target(s) in the given PinFile.
 
@@ -230,10 +229,10 @@ def up(ctx, targets, run_id, tx_id, inventory_format,
     run-id:     Use the data from the provided run_id value
     """
 
-    if ignore_failed_hooks:
-        ctx.set_cfg("hook_flags", "ignore_failed_hooks", ignore_failed_hooks)
-    if no_hooks:
-        ctx.set_cfg("hook_flags", "no_hooks", no_hooks)
+    if ifh:
+        ctx.set_cfg("hook_flags", "ignore_failed_hooks", ifh)
+    if nh:
+        ctx.set_cfg("hook_flags", "no_hooks", nh)
 
     if tx_id:
         try:
@@ -275,10 +274,10 @@ def up(ctx, targets, run_id, tx_id, inventory_format,
 @click.option('-t', '--tx-id', metavar='tx_id', type=int,
               help='Destroy resources using the transaction ID (tx-id)',
               cls=MutuallyExclusiveOption, mutually_exclusive=["run_id"])
-@click.option('-ifh', '--ignore-failed-hooks', is_flag=True)
-@click.option('-nh', '--no-hooks', is_flag=True)
+@click.option('--ifh', '--ignore-failed-hooks', is_flag=True)
+@click.option('--nh', '--no-hooks', is_flag=True)
 @pass_context
-def destroy(ctx, targets, run_id, tx_id, ignore_failed_hooks, no_hooks):
+def destroy(ctx, targets, run_id, tx_id, ifh, nh):
     """
     Destroys resources using either the run_id or tx_id (mutually exclusive).
 
@@ -293,10 +292,10 @@ def destroy(ctx, targets, run_id, tx_id, ignore_failed_hooks, no_hooks):
 
     """
 
-    if ignore_failed_hooks:
-        ctx.set_cfg("hook_flags", "ignore_failed_hooks", ignore_failed_hooks)
-    if no_hooks:
-        ctx.set_cfg("hook_flags", "no_hooks", no_hooks)
+    if ifh:
+        ctx.set_cfg("hook_flags", "ignore_failed_hooks", ifh)
+    if nh:
+        ctx.set_cfg("hook_flags", "no_hooks", nh)
 
 
     if tx_id:
