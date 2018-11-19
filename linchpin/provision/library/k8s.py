041c965e9418ddecaba5d1472f3a0c2d999f0e0a
Samvaran Kashyap Rallabandi <samvaran.kashyap@gmail.com>
2018-11-19 09:12:51 -0500

    Updated k8s module to w.r.to flake8 conventions

--- linchpin/provision/library/k8s.py
+++ linchpin/provision/library/k8s.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 # (c) 2018, Chris Houseknecht <@chouseknecht>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+from ansible.module_utils.k8s.raw import KubernetesRawModule
 
 __metaclass__ = type
 
@@ -27,10 +29,12 @@ author:
 
 description:
   - Use the OpenShift Python client to perform CRUD operations on K8s objects.
-  - Pass the object definition from a source file or inline. See examples for reading
+  - Pass the object definition from a source file or inline.
+    See examples for reading
     files and using Jinja templates or vault-encrypted files.
   - Access to the full range of K8s APIs.
-  - Use the M(k8s_facts) module to obtain a list of items about an object of type C(kind)
+  - Use the M(k8s_facts) module to obtain a list of items
+    about an object of type C(kind)
   - Authenticate using either a config file, certificates, password or token.
   - Supports check mode.
 
@@ -43,15 +47,20 @@ extends_documentation_fragment:
 options:
   merge_type:
     description:
-    - Whether to override the default patch merge approach with a specific type. By default, the strategic
+    - Whether to override the default patch merge approach
+      with a specific type. By default, the strategic
       merge will typically be used.
-    - For example, Custom Resource Definitions typically aren't updatable by the usual strategic merge. You may
-      want to use C(merge) if you see "strategic merge patch format is not supported"
-    - See U(https://kubernetes.io/docs/tasks/run-application/update-api-object-kubectl-patch/#use-a-json-merge-patch-to-update-a-deployment)
+    - For example, Custom Resource
+      Definitions typically aren't updatable by the usual strategic merge.
+      You may want to use C(merge) if you see
+      "strategic merge patch format is not supported"
     - Requires openshift >= 0.6.2
-    - If more than one merge_type is given, the merge_types will be tried in order
-    - If openshift >= 0.6.2, this defaults to C(['strategic-merge', 'merge']), which is ideal for using the same parameters
-      on resource kinds that combine Custom Resources and built-in resources. For openshift < 0.6.2, the default
+    - If more than one merge_type is given,
+      the merge_types will be tried in order
+    - If openshift >= 0.6.2, this defaults to C(['strategic-merge', 'merge']),
+      which is ideal for using the same parameters
+      on resource kinds that combine Custom Resources and built-in resources.
+      For openshift < 0.6.2, the default
       is simply C(strategic-merge).
     choices:
     - json
@@ -61,21 +70,25 @@ options:
     version_added: "2.7"
   wait:
     description:
-    - Whether to wait for certain resource kinds to end up in the desired state. By default the module exits once Kubernetes has
+    - Whether to wait for certain resource kinds to end up in the desired state.
+      By default the module exits once Kubernetes has
       received the request
-    - Implemented for C(state=present) for C(Deployment), C(DaemonSet) and C(Pod), and for C(state=absent) for all resource kinds.
+    - Implemented for C(state=present) for C(Deployment),
+      C(DaemonSet) and C(Pod), and for C(state=absent) for all resource kinds.
     - For resource kinds without an implementation, C(wait) returns immediately.
     default: no
     type: bool
     version_added: "2.8"
   wait_timeout:
     description:
-    - How long in seconds to wait for the resource to end up in the desired state. Ignored if C(wait) is not set.
+    - How long in seconds to wait for the resource to end up
+      in the desired state. Ignored if C(wait) is not set.
     default: 120
     version_added: "2.8"
   validate:
     description:
-      - how (if at all) to validate the resource definition against the kubernetes schema.
+      - how (if at all) to validate the resource definition
+        against the kubernetes schema.
         Requires the kubernetes-validate python module
     suboptions:
       fail_on_error:
@@ -83,7 +96,8 @@ options:
         required: yes
         type: bool
       version:
-        description: version of Kubernetes to validate against. defaults to Kubernetes server version
+        description: version of Kubernetes to validate against.
+        defaults to Kubernetes server version
       strict:
         description: whether to fail when passing unexpected properties
         default: no
@@ -148,12 +162,14 @@ EXAMPLES = '''
 
 - name: >-
     Read definition file from the Ansible controller file system.
-    If the definition file has been encrypted with Ansible Vault it will automatically be decrypted.
+    If the definition file has been encrypted with
+    Ansible Vault it will automatically be decrypted.
   k8s:
     state: present
     definition: "{{ lookup('file', '/testing/deployment.yml') }}"
 
-- name: Read definition file from the Ansible controller file system after Jinja templating
+- name: Read definition file from the Ansible controller
+  file system after Jinja templating
   k8s:
     state: present
     definition: "{{ lookup('template', '/testing/deployment.yml') }}"
@@ -177,7 +193,8 @@ EXAMPLES = '''
 RETURN = '''
 result:
   description:
-  - The created, patched, or otherwise present object. Will be empty in the case of a deletion.
+  - The created, patched, or otherwise present object.
+    Will be empty in the case of a deletion.
   returned: success
   type: complex
   contains:
@@ -190,11 +207,13 @@ result:
        returned: success
        type: str
      metadata:
-       description: Standard object metadata. Includes name, namespace, annotations, labels, etc.
+       description: Standard object metadata.
+       Includes name, namespace, annotations, labels, etc.
        returned: success
        type: complex
      spec:
-       description: Specific attributes of the object. Will vary based on the I(api_version) and I(kind).
+       description: Specific attributes of the object.
+       Will vary based on the I(api_version) and I(kind).
        returned: success
        type: complex
      status:
@@ -202,7 +221,8 @@ result:
        returned: success
        type: complex
      items:
-       description: Returned only when multiple yaml documents are passed to src or resource_definition
+       description: Returned only when multiple yaml
+       documents are passed to src or resource_definition
        returned: when resource_definition or src contains list of objects
        type: list
      duration:
@@ -212,7 +232,6 @@ result:
        sample: 48
 '''
 
-from ansible.module_utils.k8s.raw import KubernetesRawModule
 
 
 def main():
