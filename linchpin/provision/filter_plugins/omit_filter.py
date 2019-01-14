80540ce000cb7bf3629d01a1a113088769e8d1d9
Samvaran Kashyap Rallabandi <samvaran.kashyap@gmail.com>
2019-01-14 02:16:16 -0500

    Add omit filter to check variable is null string or not

---
+++ linchpin/provision/filter_plugins/omit_filter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import json
+
+
+def format_output(output, omit):
+    if output == "":
+        return omit
+    return output
+
+
+class FilterModule(object):
+    ''' A filter to fix output format '''
+    def filters(self):
+        return {
+            'omit_filter': format_output
+        }
