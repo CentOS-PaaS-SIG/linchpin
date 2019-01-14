a468cc2f711814f742e314ab8ea049741aa22d39
Ryan Cole <rycole@redhat.com>
2019-01-14 11:59:39 -0500

    Make openshift check pods for state instead of containers
    Signed-off-by: Ryan Cole <rycole@redhat.com>

--- linchpin/provision/filter_plugins/get_pod_status.py
+++ linchpin/provision/filter_plugins/get_pod_status.py
@@ -4,19 +4,17 @@
 def get_pod_status(pod_list, res_def_out):
     # this filter runs only when the kind is "Pod"
     pod_status = pod_list.get("resources", [])
-    container_names = [x["name"]
-                       for x in res_def_out["result"]["spec"]["containers"]]
     pod_stats = {}
     # filters out all the pods data w.r.to containers
     # provisioned in pods
     for x in pod_status:
-        if x["metadata"]["name"] in container_names:
+        if x["metadata"]["name"] == res_def_out["result"]["metadata"]["name"]:
             pod_data = x
             pod_data["status"] = x["status"]["phase"]
             pod_stats[x["metadata"]["name"]] = pod_data
-    # checks for the status of the container and returns Failure value
-    # when any container is not in Running state
-    for name in container_names:
+    # checks for the status of the pod and returns Failure value
+    # when any pod is not in Running state
+    for name in pod_stats.keys():
         if pod_stats[name]["status"] != "Running":
             return "Failure"
     return pod_stats
