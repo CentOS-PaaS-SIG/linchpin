5ea59b210139faf45b8695c83119359db5ce8498
Samvaran Kashyap Rallabandi <samvaran.kashyap@gmail.com>
2019-01-21 03:53:05 -0500

    PEP8 fixes

--- linchpin/provision/filter_plugins/fetch_attr.py
+++ linchpin/provision/filter_plugins/fetch_attr.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-import json
-
 
 def fetch_attr(output_dict, attr, default):
     return output_dict.get(attr, default)
 
 
 class FilterModule(object):
-    ''' A filter to fetch from dict '''
+    ''' A filter to fetch ann attr from dict '''
     def filters(self):
         return {
             'fetch_attr': fetch_attr
