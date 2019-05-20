33046777ee3b1df840a170691a230884195dcc48
14rcole <rycole@redhat.com>
2019-05-20 19:12:24 +0000

    Add script to cleanup old openstack instances
    Signed-off-by: 14rcole <rycole@redhat.com>

---
+++ config/Dockerfiles/cleanup-openstack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for server in `openstack server list --name ci-lp-cp* -c ID --format value`; do
+    start=$(openstack server show -c OS-SRV-USG:launched_at --format value $server)
+    uptime=$(($(date +%s)-$(date --date "$start" +%s)))
+    # delete instance if it is more than 6hrs old
+    if [ $uptime -gt 21600 ]; then
+        # openstack server delete $server
+        openstack server delete $server
+    fi
+done
