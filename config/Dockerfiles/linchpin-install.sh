1122dd67e2af2ca441121df40cbc75854646ba83
Samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2019-08-16 12:05:46 -0400

    Update linchpin-install.sh to make use of virtualenv

--- config/Dockerfiles/linchpin-install.sh
+++ config/Dockerfiles/linchpin-install.sh
@@ -1,21 +1,26 @@
 #!/bin/bash -x
 
 WORKDIR=$(pwd)
-virtualenv linchpin_venv;
-source linchpin_venv/bin/activate;
 
-echo "Present working directory is ...."
-echo "${pwd}"
-pushd config/Dockerfiles
-make pip_install.sh
-./pip_install.sh
-popd
+find / -name "virtualenv"
 
-pip install . --ignore-installed --user
-pip install .[tests] --user
-pip install .[libvirt] --user
-pip install .[beaker] --user
-pip install .[docker] --user
+/usr/bin/virtualenv linchpin_venv;
+source ./linchpin_venv/bin/activate;
+
+#echo "Present working directory is ...."
+#echo "${pwd}"
+#pushd config/Dockerfiles
+#make pip_install.sh
+#./pip_install.sh
+#popd
+
+pip install -e file://$PWD
+
+pip install . --ignore-installed 
+pip install .[tests]
+pip install .[libvirt]
+pip install .[beaker]
+pip install .[docker]
 
 # If duffy.key is available then install duffy ansible module.
 if [ -e "keys/duffy.key" ]; then
