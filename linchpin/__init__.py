2f456fe87b7f7ff50ce997a06d9cf7aaf0f0e496
Samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2019-10-04 11:30:55 -0400

    Update import statements

--- linchpin/__init__.py
+++ linchpin/__init__.py
@@ -19,8 +19,7 @@ from linchpin.ansible_runner import ansible_runner
 from linchpin.hooks.state import State
 from linchpin.hooks import LinchpinHooks
 
-from linchpin.rundb.basedb import BaseDB
-from linchpin.rundb.drivers import DB_DRIVERS
+from ansible.module_utils.linchpin_rundb import BaseDB, DB_DRIVERS
 
 from linchpin.exceptions import ActionError
 from linchpin.exceptions import LinchpinError
