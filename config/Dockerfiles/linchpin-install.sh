753f54a559bbe003f9a04c6ac4db44ac6100e6f0
Samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2019-10-21 09:11:25 -0400

    Move pip_install.sh into linchpin-install.sh

--- config/Dockerfiles/linchpin-install.sh
+++ config/Dockerfiles/linchpin-install.sh
@@ -1,11 +1,29 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 WORKDIR=$(pwd)
 
-pushd config/Dockerfiles
-make pip_install.sh
-./pip_install.sh
-popd
+pip3 install 'setuptools>=40.8.0'
+pip3 install 'pyopenssl>=17.5.0'
+pip3 install 'boto>=2.49.0'
+pip3 install 'boto3>=1.9.96'
+pip3 install 'apache-libcloud>=0.20.1'
+pip3 install 'click>=7.0'
+pip3 install 'yamlordereddictloader>=0.4.0'
+pip3 install 'ansible>=2.7.1,<=2.9.0'
+pip3 install 'six>=1.10.0'
+pip3 install 'shade>=1.30.0'
+pip3 install 'naked>=0.1.31'
+pip3 install 'Cerberus>=1.2'
+pip3 install 'tinydb>=3.12.2'
+pip3 install 'requests>=2.21.0'
+pip3 install 'ipaddress>=1.0.17'
+pip3 install 'urllib3>=1.23,<1.25'
+pip3 install 'PyYAML>=5.1'
+pip3 install 'jinja2>=2.10'
+pip3 install 'configparser>=3.5.0'
+pip3 install 'pyasn1>=0.1.9'
+pip3 install 'gitpython==2.1.11'
+pip3 install 'future'
 
 pip3 install . --ignore-installed
 pip3 install .[tests]
