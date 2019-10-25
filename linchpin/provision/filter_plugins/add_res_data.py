733f19ce3331943eeca0a8c7ef1bbd8e4e8b5080
Samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2019-10-25 09:56:40 -0400

    move add_res_data from FilteUtils since it is not recognized by --use-shell flag

--- linchpin/provision/filter_plugins/add_res_data.py
+++ linchpin/provision/filter_plugins/add_res_data.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
-import linchpin.FilterUtils.FilterUtils as filter_utils
+#import linchpin.FilterUtils.FilterUtils as filter_utils
+
+def add_res_data(hosts, res_grp, role):
+    new_hosts = []
+    for host in hosts:
+        host['resource_group'] = res_grp
+        host['role'] = role
+        new_hosts.append(host)
+    return new_hosts
 
 
 class FilterModule(object):
     ''' A filter to add_res_data '''
     def filters(self):
         return {
-            'add_res_data': filter_utils.add_res_data
+            'add_res_data': add_res_data
         }
