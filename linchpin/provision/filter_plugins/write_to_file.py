9eb9a2984197cab18737cfd9f218bf3a4f2f5e30
Samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2019-10-25 14:21:27 -0400

    Move write_to_file back to filter_plugins

--- linchpin/provision/filter_plugins/write_to_file.py
+++ linchpin/provision/filter_plugins/write_to_file.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
-import linchpin.FilterUtils.FilterUtils as filter_utils
+# import linchpin.FilterUtils.FilterUtils as filter_utils
+
+
+def write_to_file(data, path, filename):
+    filename = filename.replace(' ', '_').lower()
+
+    fd = open(path + filename + ".output", "w")
+    fd.write(json.dumps(data))
+    fd.close()
+    return data
 
 
 class FilterModule(object):
     ''' A filter to write variables to file '''
     def filters(self):
         return {
-            'write_to_file': filter_utils.write_to_file
+            'write_to_file': write_to_file
         }
