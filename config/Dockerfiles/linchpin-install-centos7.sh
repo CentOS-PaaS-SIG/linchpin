833e315db155e45c0c2798d9f05d3c16d820677c
Samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2019-10-25 09:38:31 -0400

    Add seprate install script for centos7

---
+++ config/Dockerfiles/linchpin-install-centos7.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ex
+
+WORKDIR=$(pwd)
+
+pushd config/Dockerfiles
+make pip_install.sh
+./pip_install.sh
+popd
+
+pip install . --ignore-installed
+pip install .[tests]
+pip install .[libvirt]
+pip install .[beaker]
+pip install .[docker]
+pip install .[azure]
+pip install .[openshift]
+
+# If duffy.key is available then install duffy ansible module.
+if [ -e "keys/duffy.key" ]; then
+    # duffy key needs to be in home dir by default
+    #cp duffy ~/duffy,key
+
+    # Link duffy module linchpin library
+    pushd ~
+    linchpin_path=$(python -c 'import os, linchpin; print os.path.dirname(linchpin.__file__)')
+    popd
+    if [ -n "$linchpin_path" ]; then
+        pushd $linchpin_path/provision/library
+        ln -s $WORKDIR/duffy-ansible-module/library/duffy.py .
+        popd
+    fi
+fi
+
