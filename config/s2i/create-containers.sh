0f19a98679cfffb320c2889d33259779b416a203
Samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2019-11-21 10:49:34 -0500

    Remove fedora29 from the container integration tests

--- config/s2i/create-containers.sh
+++ config/s2i/create-containers.sh
@@ -19,8 +19,7 @@ templates="jenkins/jenkins-continuous-infra-slave-buildconfig-template.yaml \
     jenkins/jenkins-persistent-buildconfig-template.yaml \
     jenkins/buildah-buildconfig-template.yaml \
     distros/centos7-buildconfig-template.yaml \
-    distros/fedora30-buildconfig-template.yaml \
-    distros/fedora29-buildconfig-template.yaml"
+    distros/fedora30-buildconfig-template.yaml"
 
 function logerror {
   echo "Error: $1"
