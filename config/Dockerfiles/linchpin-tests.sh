f32df0b56877f4a3f437fe27a623db8ea7eb55bc
Junqi Zhang <junqzhan@redhat.com>
2019-12-06 19:06:09 -0500

    Fixed the subscriptionid

--- config/Dockerfiles/linchpin-tests.sh
+++ config/Dockerfiles/linchpin-tests.sh
@@ -83,6 +83,7 @@ result=0
 
 export CREDS_PATH="$base_dir/keys"
 pushd "${CREDS_PATH}"
+subscriptionid="$(sed 's/subscription_id: //' azure.key |tail -n 1)"
 for provider in ${PROVIDERS}; do
     # If CREDS_PATH provides a tarball extract it and
     # run it's install script
@@ -109,7 +110,6 @@ for testdir in *; do
 
                     tname="${provider}/${test}"
                     testname=${distro}/${tname}
-                    subscriptionid=$(tail -n 1 azure.key)
                     echo >> ${base_dir}/${distro}_logs/${provider}.log
                     echo "==== TEST: ${testname} ====" | tee -a ${base_dir}/${distro}_logs/${provider}.log
                     pushd "${base_dir}" &> /dev/null
