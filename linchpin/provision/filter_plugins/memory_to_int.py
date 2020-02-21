07d814b2ac0e0efdf91b311f1f6d8e42d74e25a0
Samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2020-02-21 10:08:18 -0500

    Flake8 fixes to make github linting actions to succeed

--- linchpin/provision/filter_plugins/memory_to_int.py
+++ linchpin/provision/filter_plugins/memory_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from string import digits, ascii_letters
 
+
 def memory_to_int(memory):
     amount = ''.join(c for c in memory if c in digits)
     amount = int(amount)
@@ -8,6 +9,7 @@ def memory_to_int(memory):
     if size == 'Gi':
         amount = amount * 1000
     return amount
+
 
 class FilterModule(object):
     ''' A filter to add_res_data '''
