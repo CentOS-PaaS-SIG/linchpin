360c2d1651b601ee18da62a38e046f761301ee55
Junqi Zhang <junqzhan@redhat.com>
2020-03-16 09:27:34 -0400

    delete run.sh

--- config/Dockerfiles/tests.d/run.sh
+++
@@ -1,6 +0,0 @@
-pushd /home/junqizhang/myWork/linchpin
-path=$(ls config/Dockerfiles/tests.d/azure/)
-for file in $path; do
-    /home/junqizhang/myWork/linchpin/config/Dockerfiles/tests.d/azure/${file} fedora31 azure
-    pushd /home/junqizhang/myWork/linchpin
-done
